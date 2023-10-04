5093dbd58efdd2c9f6ac189afdb62af016731ad5
Alex Kagozi <alexkagozi@gmail.com>
2023-10-04 21:17:58 +0300

    Completed Rand String Generator

--- password_generator/lib/password_generator.ex
+++ password_generator/lib/password_generator.ex
@@ -47,12 +47,12 @@ defmodule PasswordGenerator do
     {:error, "Only integers allowed for length"}
   end
   defp validate_length_is_integer(true, _options) do
-    length = options["length"] | > String.trim() |> String.to_integer()
+    length = options["length"] |> String.trim() |> String.to_integer()
     options_without_length = Map.delete(options, "length")
     options_values = Map.values(options_without_length)
     value =
       options_values
-      |> Enum.all?(fn x -> String.to_atom(x) |> is_boolean())
+      |> Enum.all?(fn x -> String.to_atom(x) |> is_boolean() end)
     validate_options_values_are_boolean(value, length, options_without_length)
   end
   defp validate_options_values_are_boolean(false, _length, _options) do
@@ -60,6 +60,47 @@ defmodule PasswordGenerator do
   end
   defp validate_options_values_are_boolean(true, _length, _options) do
      options = included_options(options)
-     
+     invalid_options? = options |> Enum.any?(&(&1 not in @allowed_options))
+     validate_options(invalid_options?, length, options)
+     strings = included ++ random_strings
+     get_result(strings)
+  end
+  defp get_result(string) do
+    string =
+      strings
+      |> Enum.shuffle()
+      |> to_string()
+  end
+  defp validate_options(false, length, options) do
+    generate_strings(length, options)
+  end
+
+  defp generate_strings(length, options) do
+    options = [:lower_case_letter | options]
+    included = include(options)
+    length = length - length(included)
+    random_strings = generate_random_strings(length, options)
+  end
+
+  defp include(options) do
+    options
+    |> Enum.map(&get(&1))
+  end
+
+  defp get(:lowercase_letter) do
+    <<Enum.random(?a..?z)>>
+  end
+
+  def generate_random_string(length, options) do
+    Enum.map(1..length, fn ->
+      Enum.random(options) |> get()
+    end)
+  end
+
+  defp included_options(options) do
+    Enum.filter(options, fn { key, value} ->
+      value |> String.trim() |> String.to_existing_atom()
+    end)
+    |> Enum.map(fn {key, _value} -> String.to_atom(key) end)
   end
 end
